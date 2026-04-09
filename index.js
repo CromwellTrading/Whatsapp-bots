b05c74571510a210a48bbbf3f0a230055c2e15e9
CromwellTrading <tradingcromwell@gmail.com>
2026-04-09 19:10:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,15 @@ app.get('/', (req, res) => res.redirect('/login.html'));
 
 app.listen(PORT, async () => {
   console.log(`🌐 Servidor corriendo en http://localhost:${PORT}`);
+
+  // Agrega esto en index.js antes de las rutas estáticas
+app.get('/config.js', (req, res) => {
+  res.setHeader('Content-Type', 'application/javascript');
+  res.send(`
+    window.SUPABASE_URL = "${process.env.SUPABASE_URL}";
+    window.SUPABASE_ANON_KEY = "${process.env.SUPABASE_ANON_KEY}";
+  `);
+});
   
   // Iniciar gestor de instancias (levanta conexiones existentes)
   await initManager();
