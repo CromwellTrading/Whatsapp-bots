648acf91bc828025acffa27650bcca5431a27d5e
CromwellTrading <tradingcromwell@gmail.com>
2026-04-09 19:11:53 -0400

    Delete public/config.js

--- public/config.js
+++
@@ -1,8 +0,0 @@
-// Agrega esto en index.js antes de las rutas estáticas
-app.get('/config.js', (req, res) => {
-  res.setHeader('Content-Type', 'application/javascript');
-  res.send(`
-    window.SUPABASE_URL = "${process.env.SUPABASE_URL}";
-    window.SUPABASE_ANON_KEY = "${process.env.SUPABASE_ANON_KEY}";
-  `);
-});
